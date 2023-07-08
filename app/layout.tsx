4b8c89cc0aa768dd1ea9bc33b6ba8c8ef62aa2d8
Isidoros Parlamas <horistics@gmail.com>
2023-07-08 08:47:41 +0200

    y016

--- app/layout.tsx
+++ app/layout.tsx
@@ -17,7 +17,7 @@ export default function RootLayout({
           className="mx-auto"
           alt={"logo"}
   />*/}
-        <span style={{ color: "lime" }}>D & H</span>
+        <span style={{ color: "beige" }}>D & H</span>
         <Link href="/">
           <h1 className="text-2xl text-white font-bold mt-4">Horistics Blog</h1>
         </Link>
