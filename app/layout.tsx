25aebd15aebc936e4325f0f3b20af62dc2f4e2a0
Isidoros Parlamas <horistics@gmail.com>
2023-07-08 08:22:02 +0200

    y012

--- app/layout.tsx
+++ app/layout.tsx
@@ -10,13 +10,13 @@ export default function RootLayout({
   const header = (
     <header>
       <div className="text-center bg-slate-800 p-8 my-6 rounded-md">
-        <Image
+        {/*<Image
           src="/logo.png"
           width={40}
           height={40}
           className="mx-auto"
           alt={"logo"}
-        />
+  />*/}
         <Link href="/">
           <h1 className="text-2xl text-white font-bold mt-4">Horistics Blog</h1>
         </Link>
